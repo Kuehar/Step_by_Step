a449ac44166b7ab62bc66b0c7b56f3b9082283cd
kuehar <green95251@gmail.com>
2021-06-05 01:52:26 +0900

    Initial Commit

---
+++ Step/StepApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepApp.swift
+//  Step
+//
+//  Created by kuehar on 2021/06/05.
+//
+
+import SwiftUI
+
+@main
+struct StepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
