f6959e1308642a8ee0562f5e8a56fc71a12bfa62
kuehar <green95251@gmail.com>
2021-06-11 20:34:17 +0900

    added animation code

--- Step/ContentView.swift
+++ Step/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isClicked: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader{
+            geometry in
+            ZStack(alignment: .top,content:{
+            Button(action: self.animation, label:{
+                Image(systemName: isClicked ? "gobackward": "plus.circle.fill")
+                    .resizable().frame(width:60,height:60,alignment: .top)
+            }).rotationEffect(isClicked ? .degrees(90) : .degrees(0))
+                    .animation(.spring())
+                    .offset(x:0,y:isClicked ? geometry.size.height - 400 : geometry.size.height - 200)
+            
+                Color
+                    .black
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                    .rotation3DEffect(
+                        isClicked ?
+                            .degrees(360) : .degrees(0),
+                        axis: (x: 1.0, y: 1.0, z: 1.0))
+                    .offset(x:0,y:isClicked ? geometry.size.height  - 300: geometry.size.height  - 100)
+                    .animation(.linear(duration: 1.0))
+            }).frame(width: geometry.size.width, height: geometry.size.height, alignment: .top)
+        }
+    }
+    func animation(){
+        self.isClicked.toggle()
     }
 }
 
